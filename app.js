28d676b790ab8a8ccef6da1ee636e2128a85d88d
nadalpeng000 <nadalpeng@gmail.com>
2023-05-10 22:06:19 +0800

    feat:initial project by express handlebars mongodb

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const app = express()
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('the app is running on http://localhost:3000')
+})
