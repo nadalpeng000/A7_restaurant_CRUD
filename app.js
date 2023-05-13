bce74429aba5cb3c91e25a7788f78b7953a22a44
nadalpeng000 <nadalpeng@gmail.com>
2023-05-13 18:54:57 +0800

    feat: add create restaurants function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 const app = express()
 const Restaurant = require('./models/restaurant')
 
@@ -23,6 +23,7 @@ db.once('open', () => {
 // require template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({extended:true}))
 
 // setting static files
 app.use(express.static('public'))
@@ -32,6 +33,17 @@ app.get('/', (req, res) => {
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
+})
+
+app.get('/restaurants/new', (req,res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req,res) => {
+  const newRestaurant = req.body
+  return Restaurant.create(newRestaurant)
+  .then(() => res.redirect('/'))
+  .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
